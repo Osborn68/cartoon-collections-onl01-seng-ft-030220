4707d1153151e6960b8f752460a387927e2924e0
Christopher Osborn <z68ozz@gmail.com>
2020-03-12 02:47:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,8 @@ def roll_call_dwarves(dwarves)
 end
 
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteers_calls)
+  planeteers_calls.map { |call| call.apitalize + "!" }
 end
 
 def long_planeteer_calls# code an argument here
