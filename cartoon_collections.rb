bc35246987161f032ed31db815fe356bd8f79297
Christopher Osborn <z68ozz@gmail.com>
2020-03-12 02:38:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves
+ name_list = []
+ names = ["Doc", "Dopey", "Bashful", "Grumpy"]
+ index = 1
+ names.each.with_index do |index,names|
+   name_list.push (#"{index}", "#{names}")
+  index += 1
 end
+end
+
 
 def summon_captain_planet# code an argument here
   # Your code here
@@ -10,7 +17,11 @@ def long_planeteer_calls# code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |item|
+    cheese_types.include?(item)
+  end
 end
+  
